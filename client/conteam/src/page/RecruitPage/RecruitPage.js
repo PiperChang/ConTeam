353c623ceb10f6775d10881fbfc2f71a45dac2f4
PiperChang <goddhqj8000@ajou.ac.kr>
2022-03-30 10:10:13 +0900

    Infinite scroll

--- client/conteam/src/page/RecruitPage/RecruitPage.js
+++ client/conteam/src/page/RecruitPage/RecruitPage.js
@@ -1,25 +1,39 @@
 import React from 'react'
 
-const ProfileCards = () =>{
-	const profiles = [1,2,3,4,5]
-	const profileCards = profiles.map((profile,index) =>(
-		<li key={index} className="bg-blue-500" >				
+const ProfileCards = () => {
+	const profiles = [1, 2, 3, 4, 5]
+	const profileCards = profiles.map((profile, index) => (
+		<li key={index} className="bg-blue-500" style={{ height: "500px", width: "300px", color: 'blue', backgroundColor : "skyblue", marginBottom:"10px" }}>
 			{profile}
 		</li>
 	))
 	return (
-			<ul>
-				{profileCards}
-			</ul>
+		<ul>
+			{profileCards}
+		</ul>
 	)
 }
 
 export default function RecruitPage() {
+	const intersectionObserver = new IntersectionObserver((entries, observer) => {
+		const ioTarget = entries[0].target;
+		if (entries[0].isIntersecting) {
+			console.log(ioTarget);
+			ioTarget.unobserve($li)
+
+			$li = $ul.appendChild
 			
-  return (
-    <div>
-		<ProfileCards />
-    </div>
-  )
+
+
+
+
+			
+		}
+	}, options)
+	return (
+		<div>
+			<ProfileCards />
+		</div>
+	)
 }
 
