a8fb759e85236d17465643a2a39444313059bc10
ws8313 <ws8313@naver.com>
2022-04-04 23:21:38 +0900

    mainpage

--- client/conteam/src/component/Main3.js
+++ client/conteam/src/component/Main3.js
@@ -1,42 +1,48 @@
 import React from "react";
 
-export default function Main2() {
-    const teams = [
+export default function Main3() {
+    const mates = [
         {
             no: "1",
-            name: "PLAN UG",
-            img: "이미지1",
-            title: "글로벌 외주회사 기획자 모집중",
+            name: "김OO",
+            profile: "이미지1",
+            address: "서울특별시 성북구",
+            age: "28세",
+            title: "기획자, 개발자 가능",
             position: "기획자",
-            text: "외주/ 상용화 단계/ 현재 구성원 1명/",
+            text: "2년차/ 창업 경험/ 브랜딩/ 개발 경험/",
             date: "2시간전"
         },
         {
             no: "2",
-            name: "참스틱스",
-            img: "이미지2",
-            title: "디자인/예술 기반 아트 커머스 플랫폼",
+            name: "김OO",
+            profile: "이미지2",
+            address: "서울특별시 성북구",
+            age: "28세",
+            title: "기획자, 개발자 가능",
             position: "기획자",
-            text: "커머스/ 제품, 서비스 확장 단계/ 현재 구성원 5명/",
-            date: "5시간전"
+            text: "2년차/ 창업 경험/ 브랜딩/ 개발 경험/",
+            date: "4시간전"
         },
         {
             no: "3",
-            name: "플레이웰",
-            img: "이미지3",
-            title: "사용자 고유명사들을 바탕으로 취향저격 장소...",
-            position: "개발자",
-            text: "여행서비스/ 프로토타입 완성 단계/ 현재 구성원 5명/",
+            name: "김OO",
+            profile: "이미지3",
+            address: "서울특별시 성북구",
+            age: "28세",
+            title: "기획자, 개발자 가능",
+            position: "기획자",
+            text: "2년차/ 창업 경험/ 브랜딩/ 개발 경험/",
             date: "7시간전"
         }
     ]
     
-    const teamList = teams.map((team) => (
+    const mateList = mates.map((mate) => (
             <div className="sm:px-3 sm:basis-full sm:max-w-full md:px-3.5 md:basis-1/2 md:max-w-[50%] lg:px-4 lg:basis-1/3 lg:max-w-[33.333333%]">
                 <div className="w-full h-full">
                     <div className="h-80 border border-black rounded-lg">
                         <div className="h-1/3 border-b border-black text-center">
-                            {team.img}
+                            {/* {team.img} */}
                         </div>
                         <div className="mt-3">
                             <div className="flex flex-col px-4">
@@ -44,23 +50,23 @@ export default function Main2() {
                                     <div className="w-full h-full space-y-6">
                                         <div className="mt-4">
                                             <div className="text-md font-medium ">
-                                                {team.title}
+                                                {/* {team.title} */}
                                             </div>
                                             <div className="text-sm font-normal ">
-                                                by {team.name}
+                                                {/* by {team.name} */}
                                             </div>
                                         </div>
                                         <div>
                                             <div className="text-md font-medium mb-2">
-                                                {team.position}
+                                                {/* {team.position} */}
                                             </div>
                                             <div className="text-sm font-normal text-gray-700">
-                                                {team.text}
+                                                {/* {team.text} */}
                                             </div>
                                         </div>
-                                        <div className="hidden 2xl:block">
+                                        <div>
                                             <div className="text-xs font-normal text-gray-500">
-                                                {team.date}
+                                                {/* {team.date} */}
                                             </div>
                                         </div>
                                     </div>
@@ -99,7 +105,7 @@ export default function Main2() {
                                             </div>
                                         </div>
                                     </div> */}
-                                    {teamList}
+                                    {mateList}
                                 </div>
                             </div>
                         </div>
