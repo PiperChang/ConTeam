a3d1bd492061aa9cae6afa91340932d27082cb04
ws8313 <ws8313@naver.com>
2022-04-10 23:02:10 +0900

    feat: add SNS login button

--- client/conteam/src/component/modals/Modal.js
+++ client/conteam/src/component/modals/Modal.js
@@ -11,13 +11,21 @@ const Modal = ({ showModal, closeModal }) => {
                             <div className="w-full h-full min-h-[695px] flex flex-row rounded justify-between bg-white">
                                 <div className="flex flex-col w-[360px] py-11 px-14 justify-center items-center text-center flex-[1_0_auto]">
                                     <h2 className="text-xl">간편가입 / 로그인</h2>
-                                    <div className="block mt-12">
+                                    <div className="block w-80 mt-12">
                                         <Link className="block w-full h-14 leading-[56px] rounded bg-[#4267B2] text-white font-medium" to="/">
-                                            <div className=""></div>
+                                            <img className="inline w-6 h-6 mx-2"
+                                            alt="facebook_icon" src="facebook_icon.svg" />
                                             페이스북 계정으로 로그인 하기
                                         </Link>
-                                        <Link className="" to="/">
-                                            <img alt="kakao_login_btn" src="kakao_login.png" />
+                                        <Link className="block w-full h-14 mt-3 leading-[56px] rounded bg-[#FEE500] text-[#381F1F] font-medium" to="/">
+                                            <img className="inline w-10 h-10"
+                                            alt="kakaotalk_icon" src="kakaotalk_icon.svg" />
+                                            카카오톡 계정으로 로그인 하기
+                                        </Link>
+                                        <Link className="block w-full h-14 mt-3 leading-[56px] rounded bg-[#03C75A] text-white font-medium" to="/">
+                                            <img className="inline w-10 h-10"
+                                            alt="naver_icon" src="naver_icon.svg" />
+                                            네이버 계정으로 로그인 하기
                                         </Link>
                                     </div>
                                 </div>
