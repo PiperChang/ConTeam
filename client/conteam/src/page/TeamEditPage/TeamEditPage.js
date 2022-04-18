84e73bf282be409343156e4e1e463c94fcc15173
KCHamp <goddhqj8000@ajou.ac.kr>
2022-04-18 21:42:03 +0900

    teameditpage

---
+++ client/conteam/src/page/TeamEditPage/TeamEditPage.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Header from '../../component/header/Header'
+
+export default function TeamEditPage() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <form>
+          <label for="team_name">팀명</label>
+          <input type="text" id='team_name' name='team_name'/>
+          <label for="short_description">프로젝트 한 줄 설명</label>
+          <input type="text" id='short_description' name='short_description'/>
+          <label for="category">컨텐츠 분야</label>
+          <select id = "category">
+            <option>웹드라마</option>
+            <option>단편 영화</option>
+            <option>코믹</option>
+            <option>교육</option>
+          </select>
+          <label for="teammates">현재 팀원</label>
+          <input type="number" id='teammates' name='teammates'/>
+          {/* 팀원 아이디 치면, 해당 팀원 프로필
+          현재 팀원 수 (@아이디+ 역할,@아이디+ 역할,@아이디+ 역할) */}
+          {/* 서비스 단계 ( 기획, 팀 모집중, 제작 중 ) */}
+          <label for="due_date">모집 마감일</label>
+          <input type="date" id='due_date' name='due_date'/>
+          <label for="active_period">예상 제작 기간</label>
+          <input type="number" id='active_period' name='active_period'/>
+          <label for="opening_position">모집 인원</label>
+          <input type="number" id='opening_position' name='opening_position'/>
+          <label for="opening_position">컨텐츠 상세 소개</label>
+          <input type="number" id='opening_position' name='opening_position'/>
+          이거 된거 맞음?
+        </form>
+      </div>
+    </div>
+  )
+}
