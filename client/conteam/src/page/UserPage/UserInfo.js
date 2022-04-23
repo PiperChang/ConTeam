029d197cc02f819df8935b5171895a0722d1cd03
ws8313 <ws8313@naver.com>
2022-04-23 23:54:07 +0900

    UserInfo role tag

--- client/conteam/src/page/UserPage/UserInfo.js
+++ client/conteam/src/page/UserPage/UserInfo.js
@@ -1,11 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function UserInfo() {
+    const [role, setRole] = useState("planner")
+    const [selectRole, setSelectRole] = useState([]);
+
+    const roleChangeHandler = (e) => {
+        setRole(e.target.value)
+        console.log(e.target.value)
+    }
+
+    const roleClickHandler = (e) => {
+        if (selectRole.length === 2) {
+            alert("세부 역할은 최대 2가지까지 선택 가능합니다.")
+            console.log(selectRole)
+        } else {
+            console.log(e.target.getAttribute("value"))
+            setSelectRole([...selectRole, e.target.getAttribute("value")])
+        }
+    }
+
+    const roleTagChangeHandler = (e) => {
+        if (selectRole.length === 3) {
+            alert("세부 역할은 최대 2가지까지 선택 가능합니다.")
+            // setSelectRole
+        } else {
+            const role_tag_list = e.target.value
+            console.log(role_tag_list)
+            const a = role_tag_list.split(",")
+            console.log(a)
+            setSelectRole(a)
+            console.log(e.target.value)
+        }
+    }
+
+    useEffect(() => {
+        if (selectRole.length === 3) {
+            alert("세부 역할은 최대 2가지까지 선택 가능합니다.")
+        }
+        console.log(selectRole)
+    }, [role, selectRole])
+
+    const planner_tag = ["장르1", "장르2", "장르3", "장르4", "장편", "단편"]
+    const staff_tag = ["촬영자", "편집자", "연출자"]
+    const cast_tag = ["배우", "코미디언", "가수", "아역배우", "동물배우"]
+
+    function role_tag() {
+        if (role === "planner") {
+            return planner_tag.map((tag, index) => (
+                <span key={index} value={tag} className="text-left w-auto px-4 py-2 mx-1 my-2 text-sm border border-gray-400 text-gray-500 rounded-md box-border inline-block cursor-pointer hover:border-gray-900 hover:text-gray-900" onClick={ roleClickHandler }>{tag}</span>
+            ))
+        } else if (role === "staff") {
+            return staff_tag.map((tag, index) => (
+                <span key={index} value={tag} className="text-left w-auto px-4 py-2 mx-1 my-2 text-sm border border-gray-400 text-gray-500 rounded-md box-border inline-block cursor-pointer hover:border-gray-900 hover:text-gray-900" onClick={ roleClickHandler }>{tag}</span>
+            ))
+        } else if (role === "cast") {
+            return cast_tag.map((tag, index) => (
+                <span key={index} value={tag} className="text-left w-auto px-4 py-2 mx-1 my-2 text-sm border border-gray-400 text-gray-500 rounded-md box-border inline-block cursor-pointer hover:border-gray-900 hover:text-gray-900" onClick={ roleClickHandler }>{tag}</span>
+            ))
+        } else {
+            return null;
+        }
+    }
+
     return (
         <div className="text-center">
             <div className="relative inline-block text-left w-[650px] mt-14 pb-9">
                 <span className="absolute inline-block text-left top-6 right-16 w-60 h-60 rounded-full overflow-hidden hover:opacity-70">
-                    <label for="profile_photo" className="cursor-default text-left">
+                    <label className="cursor-default text-left">
                         <div className="absolute opacity-0 hover:opacity-90  top-0 text-center cursor-pointer duration-300 w-full h-60 rounded-[50%]">
                             <span className="mt-24 ml-1 w-16 h-10 tracking-widest font-semibold text-center leading-10 inline-block text-xl text-white">EDIT</span>
                         </div>
@@ -58,7 +119,7 @@ export default function UserInfo() {
                     <div className="block text-left mb-5">
                         <span className="inline-block text-left w-24 leading-10 text-sm align-top">경력</span>
                         <span className="inline-block text-left">
-                            <select type="text" placeholder="ex) 01012345678" className="w-32 h-10 box-border border-b border-gray-400 rounded px-2 text-sm text-gray-800 align-middle bg-white">
+                            <select className="w-32 h-10 box-border border-b border-gray-400 rounded px-2 text-sm text-gray-800 align-middle bg-white">
                                 <option>1년 미만</option>
                                 <option>2~3년 차</option>
                                 <option>4~5년 차</option>
@@ -71,7 +132,7 @@ export default function UserInfo() {
                     <div className="block text-left mb-5">
                         <span className="inline-block text-left w-24 leading-10 text-sm align-top">상태</span>
                         <span className="inline-block text-left">
-                            <select type="text" placeholder="ex) 01012345678" className="w-32 h-10 box-border border-b border-gray-400 rounded px-2 text-sm text-gray-800 align-middle bg-white">
+                            <select className="w-32 h-10 box-border border-b border-gray-400 rounded px-2 text-sm text-gray-800 align-middle bg-white">
                                 <option>구직 중</option>
                                 <option>구인 중</option>
                                 <option>재직 중</option>
@@ -82,13 +143,15 @@ export default function UserInfo() {
                     <div className="block text-left mb-5">
                         <span className="inline-block text-left w-24 leading-10 text-sm align-top">역할</span>
                         <span className="inline-block text-left">
-                            <select type="text" placeholder="ex) 01012345678" className="w-32 h-10 box-border border-b border-gray-400 rounded px-2 text-sm text-gray-800 align-middle bg-white">
-                                <option>기획자</option>
-                                <option>스태프</option>
-                                <option>출연진</option>
-                                <option>기타</option>
+                            <select name="role" className="w-32 h-10 box-border border-b border-gray-400 rounded px-2 text-sm text-gray-800 align-middle bg-white" value={ role } onChange={ roleChangeHandler }>
+                                <option value="planner">기획자</option>
+                                <option value="staff">스태프</option>
+                                <option value="cast">출연진</option>
+                                <option value="etc">기타</option>
                             </select>
-                            <input type="text" placeholder="최대 2가지 선택 가능 ex) 기획자, 배우" className="max-w-[420px] w-[530px] h-10 box-border border-b border-gray-400 rounded px-2 text-sm text-gray-800 align-middle bg-white ml-1"></input>
+                            <input type="text" defaultValue={ selectRole } onChange={ roleTagChangeHandler } placeholder="최대 2가지 선택 가능 ex) 기획자, 배우" className="max-w-[420px] w-[530px] h-10 box-border border-b border-gray-400 rounded px-2 text-sm text-gray-800 align-middle bg-white ml-1"></input>
+                            <div className="block"></div>
+                            { role_tag() }
                         </span>
                     </div>
                     <div className="block text-left mb-5">
@@ -108,6 +171,12 @@ export default function UserInfo() {
                         <span className="inline-block text-left">
                             <textarea type="text" placeholder="간단한 자기소개를 작성해 주세요." className="max-w-[550px] w-[630px] h-48 box-border border-b border-gray-400 rounded p-2 text-sm text-gray-800 align-middle bg-white resize-none"></textarea>
                         </span>
+                    </div>
+                </div>
+                <div className="block text-left mt-7">
+                    <div className="block text-left">
+                        <span className="inline-block box-border font-medium text-center text-sm rounded cursor-pointer align-middle bg-yellow-400 hover:bg-yellow-400/80 hover:text-black border-none duration-200 leading-10 text-gray-800 ml-24 mr-1 w-28 h-10">수정완료</span>
+                        <span className="inline-block box-border font-medium text-center text-sm rounded cursor-pointer align-middle text-gray-500 hover:text-black border-none duration-200 leading-10 w-20 mr-1">취소</span>
                     </div>
                 </div>
             </div>
