cc7a41e81815a4d8ad140cc26f158dea0561acfe
PiperChang <goddhqj8000@ajou.ac.kr>
2022-05-23 21:53:25 +0900

    Save Content Event Handler

--- client/conteam/src/page/EditTeamPage/ShowContentIntroPage.js
+++ client/conteam/src/page/EditTeamPage/ShowContentIntroPage.js
@@ -1,40 +1,27 @@
-import React, { useState,useRef} from 'react'
+import React, { useState } from 'react'
 import Header from '../../component/header/Header'
 import { AiOutlineLeft, AiOutlineRight, AiOutlineClose } from 'react-icons/ai'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { CKEditor } from '@ckeditor/ckeditor5-react'
 
 export default function EditContentInfoPage() {
 
     // imgList api로 가져와야함.
     const imglist = [1, 2, 3, 4, 5]
     const [imgNum, setImgNum] = useState(0)
-    const editorRef = useRef();
+
     // onClick()
-    
     const handleDeletePic = () => {
         imglist.splice(imgNum, 1)
     }
 
-    const [contentData, setContentData] = useState({
-        title:'',
-        subtitle:"",
-        desc:"",
-        imgs:[]
-    })
 
-    const saveContentData =() =>{
-        
-        setContentData()
-    }
+
     return (
         <div>
             <Header />
+            {/* 각 태그마다 줄바꿈 + */}
             <div className='max-w-7xl min-w-0 w-[80vw] m-auto '>
-                <form onSubmit={saveContentData}>
-                <h2 className='font-bold text-2xl py-3 border-b-[2px] mb-5'>컨텐츠 소개</h2>
-                <div className='flex justify-center'>
-                    {/* Images */}
+                <h2 className='font-bold text-2xl py-3 border-b-[2px]'>컨텐츠 소개</h2>
+                <div className='flex py-10 justify-center'>
                     <div className='mr-10 w-[30vw]'>
                         <div className='flex border items-center relative justify-between flex-auto' style={{ width: "30vw", height: "30vw" }} >
                             <button className='absolute right-px top-px'><AiOutlineClose /></button>
@@ -48,22 +35,18 @@ export default function EditContentInfoPage() {
                             ))}
                         </div>
                     </div>
-                    {/* INPUT */}
                     <div className='ml-5 w-[50vw]'>
-                        <input className='font-bold text-3xl px-2' placeholder='Title'></input>
-                        <br></br>
-                        <input className='text-xl my-2 px-2 ' placeholder='부제'></input>
-                        <textarea className='w-full mt-10 h-4/5 p-2' placeholder='컨텐츠에 대한 상세한 소개내용을 적어주세요.'/>
+                        <h1 className='font-bold text-3xl'>Title</h1>
+                        <h2 className='text-xl mt-2'>부제</h2>
+                        <p className='w-full border-t-2'>프로젝트 상세 설명</p>
                     </div>
                 </div>
-                
-                <div className='flex border-t-2 my-3 py-2'>
-                    <button type="submit" className='ml-auto shadow rounded-full p-5 text-orange-500 hover:bg-orange-300 font-bold hover:text-black '>저장</button>                        
-                    <button className='ml-5 shadow rounded-full p-5 text-orange-500 hover:bg-orange-300 font-bold hover:text-black'>다음</button>                        
+                <div>
+                    <h2 className='font-bold text-2xl py-3 border-b-[2px]'>팀 소개</h2>
+                    <div className=''>
+                        
+                    </div>
                 </div>
-            </form>
-
-{/* 컷~~~ */}
                 <h2 className='font-bold text-2xl py-3 border-b-[2px]'>컨텐츠 소개</h2>
                 <div className='flex py-10 justify-center'>
                     <div className='mr-10 w-[30vw]'>
